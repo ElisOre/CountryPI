a3a5713bded9431156922d460ea0dd8b4a2b0fe4
ElisOre <elias.orellana.31@gmail.com>
2023-04-01 23:03:00 -0300

    Tabla country lista

--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -3,36 +3,40 @@ const { DataTypes } = require("sequelize");
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define("country", {
-    id: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    "country",
+    {
+      id: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      flags: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      region: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      capital: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      subregion: {
+        type: DataTypes.STRING,
+      },
+      area: {
+        type: DataTypes.INTEGER,
+      },
+      population: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
     },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    flags: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    region: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    capital: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    subregion: {
-      type: DataTypes.STRING,
-    },
-    area: {
-      type: DataTypes.INTEGER,
-    },
-    population: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-  });
+    { timestamps: false }
+  );
 };
