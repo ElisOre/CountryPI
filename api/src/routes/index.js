53bfab4d8b368ef2fd570889540d64bf61287a0b
ElisOre <elias.orellana.31@gmail.com>
2023-04-03 08:32:02 -0300

    index de las rutas configurada

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,15 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
-
 const router = Router();
+
+const country = require('./country.js');
+const activity = require('./activity.js');
+
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
+router.use('/', country);
+router.use('/activity', activity);
 
 module.exports = router;
