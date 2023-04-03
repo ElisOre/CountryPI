d599c8648cacdc051c49214a9815c8eff3fdc642
ElisOre <elias.orellana.31@gmail.com>
2023-04-03 08:36:59 -0300

    Elimino la creación de db antes de cargar todo

--- api/index.js
+++ api/index.js
@@ -20,31 +20,9 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 
-const { Country } = require("./src/db.js");
-const axios = require("axios");
-
-// Función para llamar a la api
-async function createDB() {
-  const { data } = await axios.get("https://restcountries.com/v3/all");
-  const db = data?.map((el) => {
-    return {
-      id: el.cca3,
-      name: el.name.common,
-      flags: el.flags[0],
-      region: el.region,
-      capital: el.capital ? el.capital[0] : "Este país no tiene capital",
-      subregion: el.subregion,
-      area: el.area,
-      population: el.population,
-    };
-  });
-
-  await Country.bulkCreate(db);
-}
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(async () => {
-  await createDB();
+conn.sync({ force: true }).then(() => {
   server.listen(3001, () => {
     console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
